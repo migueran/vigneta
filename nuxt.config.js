311fb449e8132d7faf3da8ec5b146dde6cff88c2
mike! <mrando.via@gmail.com>
2019-05-01 11:27:58 -0300

    font-awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,15 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { 
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.6.0/css/all.css',
+        integrity:'sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h',
+        crossorigin: 'anonymous'
+      }
+    ]
   },
 
   /*
